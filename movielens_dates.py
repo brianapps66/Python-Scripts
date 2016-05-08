28ca71b6f62c90dcf0b8310e5956b91a3db29a15
Brian <bri257n@gmail.com>
2016-05-09 00:59:19 +0100

    File updated

--- movielens_dates.py
+++ movielens_dates.py
@@ -3,18 +3,13 @@ from datetime import datetime
 
 file = open('u.item', 'r')
 
-with open('u_date_adjusted.item', 'a')as new_file:
+with open('u_date_adjusted.item', 'w') as new_file:
     for line in file:
-        #print(line)
         regex = r'\d{2}-\w{3}-\d{4}'
         match = re.search(regex, line)
         if match is not None:
-            #print(match)
             date = datetime.strptime(match.group(), "%d-%b-%Y").date()
-            #print(date)
-           #new_date = datetime.strftime(date, "%Y-%m-%d")
-            #re.sub(regex, new_date, line)
-            re.sub(regex, str(date), line)
+            line = re.sub(regex, str(date), line)
         new_file.write(line)
 
 file.close()
